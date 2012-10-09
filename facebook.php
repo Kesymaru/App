581f1a0c5993dd59b5da9fd9c45d163a3f297230
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2012-10-09 11:14:46 -0600

    Facebook connect
    Configurado

--- facebook.php
+++ facebook.php
@@ -40,3 +40,28 @@ if (!$user) {
 }
 
 ?>
+
+<!doctype html>
+<html xmlns:fb="http://www.facebook.com/2008/fbml">
+  <head>
+    <title>php-sdk 3.0.0</title>
+        <style typ="text/css">
+            html, body { width: 520px;}
+        </style>    
+  </head>
+  <body>
+    <h1>php-sdk</h1>
+    <h3>PHP Session</h3>
+      <?php foreach($_SESSION as $key=>$value){
+          echo '<strong>' . $key . '</strong> => ' . $value . '<br />';
+      }
+      ?>
+      <h3>Tu</h3>
+      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+      <h3>Tus datos (/me)</h3>
+      <?php foreach($user_profile as $key=>$value){
+          echo '<strong>' . $key . '</strong> => ' . $value . '<br />';
+      }
+      ?>
+  </body>
+</html>
