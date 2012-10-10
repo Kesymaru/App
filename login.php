e4f9218f43a9168707317bd43f43a068df433e8b
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2012-10-10 10:24:21 -0600

    Limpiza Y funcionalidad
    Guarda en la base de datos los datos de los usuarios nuevos

--- login.php
+++ login.php
@@ -32,6 +32,7 @@ if ($user) {
 if ($user) {
 	$logoutUrl = $facebook->getLogoutUrl();
 } else {
+
 	//prmisos solicitados para la app
 	$loginUrl = $facebook->getLoginUrl(            
 	array(
@@ -45,97 +46,44 @@ if (!$user) {
         exit;
 }else{
 	//logueado
-	usuario();
+	usuario($user_profile);
 }
 
-?>
-
-<! html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml">
-  <head>
-    <title>php-sdk 3.0.0</title>
-        <style typ="text/css">
-            html, body { width: 520px;}
-        </style>    
-  </head>
-  <body>
-    <h1>php-sdk</h1>
-    <h3>PHP Session</h3>
-      <?php foreach($_SESSION as $key=>$value){
-          echo '<strong>' . $key . '</strong> => ' . $value . '<br />';
-      }
-      ?>
-      <h3>Tu</h3>
-      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
-      <h3>Tus datos (/me)</h3>
-      <?php 
-      	$keys = array_keys($user_profile);
-      	
-      	$values = array_values($user_profile);
-
-      	echo '<hr>Keys';
-      	print_r($keys);
-      	echo '<hr>Values';
-      	print_r($values);
-      	echo "<hr><center>mail 2</center>".array_search('@', $user_profile)."</hr>";
-      	echo '<hr>';
-
-      	foreach($user_profile as $key => $value){
-        	echo '<strong>' . $key . '</strong> => ' . $value . '<br />';
-      }
-      ?>
-  </body>
-</html>
-
-<?php
-
-//re direccion al home, esto limpia los parametros enviados por facebook via GET
-$home = "http://www.laquesigue.com/";
-//echo "<script type='text/javascript'>top.location.href = '$home';</script>";
-//exit;
-
+/*
+	Para limpiar los parametros enviados desde facebook via GET, se redirecciona a index.php
+	Facebook abre sesion automaticamente
+*/
+/*$home = "http://www.laquesigue.com/";
+echo "<script type='text/javascript'>top.location.href = '$home';</script>";
+exit;*/
 
 //revisa si exise el usuario
-function usuario(){
-	//$_SESSION['fb_369466053131869_user_id'] -> 369466053131869 -> numero de la app
-	$sql = 'SELECT * FROM usuarios WHERE id = '.$_SESSION['fb_369466053131869_user_id'];
+function usuario($user_profile){
+
+	$sql = 'SELECT * FROM usuarios WHERE id = '.$user_profile['id'];
 	$sql = mysql_query($sql);
 
-	//echo "<script type='text/javascript'> alert('Resultado".$user_profile as '' => $value." idFace ".$_SESSION['fb_369466053131869_user_id']."'); </script>";
+	if( mysql_fetch_array($sql) ){ //existe el usuario
+		echo "<script type='text/javascript'> alert('Existe'); </script>";
 
-	if( mysql_fetch_array($sql) ){
-		//echo "<script type='text/javascript'> alert('Existe'); </script>";
-	}else{
-		//echo "<script type='text/javascript'> alert('No existe Grabando...'); </script>";	
-		grabar();
+	}else{ //no existe
+		echo "<script type='text/javascript'> alert('No existe Grabando...'); </script>";	
+		grabar($user_profile);
 	}
 }
 
 //graba datos de usuarios en base de datos
-function grabar(){
-	//cargaDatos();
-	$sql = "INSERT INTO usuarios (nombre, apellido, email, id) VALUES (".$values[2].", ".$values[3].", $".$values[2].", ".$id.")";
-	echo array_keys($user, "id");
-	echo "<script type='text/javascript'> alert('Nombre:".array_keys($user_profile, "id")."'); </script>";
+function grabar($user_profile){
+
+	$sql = "INSERT INTO usuarios VALUES ('".$user_profile['first_name']."', '".$user_profile['last_name']."', '".$user_profile['email']."', '".$user_profile['id']."')";
 	
-	//$resultado = mysql_query($sql);
-
-	if($resultado){
+	//graba en la base de datos
+	if( !mysql_query($sql) ){
 		//echo "<script type='text/javascript'> alert('Grabado'); </script>";
+		die('Error: ' . mysql_error());
 	}else{
-		//echo "<script type='text/javascript'> alert('No grabado error bd'); </script>";
+		//echo "<script type='text/javascript'> alert('Grabado'); </script>";
 	}
-	echo "<hr>".$sql;
-}
-
-function cargaDatos(){
-	$datos = array();
-	$i = 0;
-	/*foreach($user_profile as $key => $value){
-    	$datos[$i] = $value;
-    	echo $datos[$i];
-    	$i++;s
-    }*/
 }
 
 ?>
